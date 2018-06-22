47a6f958be52f25e6d7cbd137e7d19a2356455f4
root <root@minecraft-demo.oxagile.com>
2018-06-22 10:22:07 +0300

    fix s3 module outputs

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,9 +2,3 @@ output "bucket_ids" {
     description = "bucket_id"
     value = "${aws_s3_bucket.s3_bucket.*.id}"
 }
-output "bucket_arns" {
-    value = "${aws_s3_bucket.s3_bucket.*.arn}"
-}
-output "bucket_domain_names" {
-    value = "${aws_s3_bucket.s3_bucket.*.bucket_domain_name}"
-}
